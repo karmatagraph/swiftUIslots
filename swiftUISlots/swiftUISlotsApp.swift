b2d6ac23ccdde68f875934ce2075b67b254eb273
karma <karmatashi954@gmail.com>
2022-01-11 00:13:32 +0545

    Initial Commit

---
+++ swiftUISlots/swiftUISlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUISlotsApp.swift
+//  swiftUISlots
+//
+//  Created by karma on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftUISlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
